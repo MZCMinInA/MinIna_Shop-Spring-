bdb01f28802dc90c9afdd1c594f42feb048824c6
mindun <pmk10077@gmail.com>
2025-11-05 19:17:09 +0900

    first commit

---
+++ src/main/java/com/example/min_ina_shop/MinInaShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.min_ina_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinInaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinInaShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/min_ina_shop/MinInaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.min_ina_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinInaShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
